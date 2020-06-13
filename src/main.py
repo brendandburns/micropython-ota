fe1135244a6e0c854d2d251952796a1837f7d083
Brendan Burns <brendan.d.burns@gmail.com>
2020-06-13 09:11:37 -0700

    Updates.

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ from ota import OTAUpdater
 
 
 def download_and_install_update_if_available():
-    ota_updater = OTAUpdater('https://github.com/brendandburns/micro-python')
+    ota_updater = OTAUpdater('https://github.com/brendandburns/micropython-ota')
     # We're already connected so no need to pass the real passwords
     ota_updater.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
 
 
 def start():
-    from main.main import run
+    from main.app import run
     run()
 
 def boot():
